3b0369420281fe7889474d130c2c394cdf814f8b
Ritik Vishwakarma <123971705+Ritikguru41@users.noreply.github.com>
2026-07-29 20:18:36 +0530

    Time: 1 ms (60.48%), Space: 44.5 MB (59.58%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -12,19 +12,3 @@ class Solution {
         return (larg -1) * (seclarg -1);
     }
 }
-
-    
-        // // Arrays.sort(nums);
-        // // int ans = ((nums[nums.length-1])-1) * (nums[nums.length-2]-1);
-        // // return ans;
-
-        // int larg = 0, seclarg = 0;
-        // for(int arr : n){
-        //     if(arr > larg){
-        //         seclarg = larg;
-        //         larg = arr;
-        //     }else if(arr > seclarg){
-        //         seclarg = arr;
-        //     }
-        // }
-        // return (larg - 1) * (seclarg - 1);
