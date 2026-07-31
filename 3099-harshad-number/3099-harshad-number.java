b2b09463594fcadf64bfe70552d820c8ee9b4e24
Ritik Vishwakarma <123971705+Ritikguru41@users.noreply.github.com>
2026-07-31 19:30:02 +0530

    Time: 0 ms (100%), Space: 42 MB (63.11%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.java
+++ 3099-harshad-number/3099-harshad-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-     int original = x;
-     int sum = 0;
-     while(x>0){
-        sum += x%10;
-        x /= 10;
+     int original = x;// value store
+     int sum = 0;// initiallize 0
+     while(x>0){ // loop till meet 0
+        sum += x%10;// sum + digit add
+        x /= 10; // 18 -> 8
      }
-     if(original % sum ==0) return sum;
-     else return -1;
+     if(original % sum ==0) return sum;//return the sum 
+     else return -1;// return -1
     }
 }
