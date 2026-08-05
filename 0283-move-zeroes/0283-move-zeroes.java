18cb52166f042ec9d8280c2407a88f24e4bb64e2
Ritik Vishwakarma <123971705+Ritikguru41@users.noreply.github.com>
2026-08-05 19:03:38 +0530

    Time: 136 ms (5.05%), Space: 48.1 MB (6.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,16 @@
 class Solution {
     public void moveZeroes(int[] n) {
-       int j = 0;
-       for(int i =0;i < n.length; i++){
+     int inspos = 0;
+     for(int i =0 ;i < n.length; i++){
         if(n[i] != 0){
-            int temp = n[i];
-            n[i] = n[j];
-            n[j] = temp;
-            j++;
+            if(i != inspos){
+                n[inspos] = n[i];
+                n[i] = 0;
+            }
+            inspos++;
         }
-       }
-       for(int nums:n)
-       System.out.print(nums + " ");
+     }
+     for(int arr: n)
+     System.out.println(arr);
     }
 }
