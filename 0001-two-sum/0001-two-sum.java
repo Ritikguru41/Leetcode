29c37b6f434220839e5dbfdb962d63e2592433c2
Ritik Vishwakarma <123971705+Ritikguru41@users.noreply.github.com>
2026-08-15 12:55:53 +0530

    Time: 9 ms (46.31%), Space: 46.9 MB (74.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] n, int target) {
-        for(int i =0; i < n.length;i++){
-            for(int j = i+1; j < n.length; j++){
-                if(n[i] + n[j] == target)
-                return new int[]{i, j};
+
+        // Store: [value, original index]
+        int[][] arr = new int[n.length][2];
+
+        for (int i = 0; i < n.length; i++) {
+            arr[i][0] = n[i];
+            arr[i][1] = i;
+        }
+
+        // Sort according to value
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int i = 0;
+        int j = n.length - 1;
+
+        while (i < j) {
+
+            int sum = arr[i][0] + arr[j][0];
+
+            if (sum == target) {
+                return new int[]{arr[i][1], arr[j][1]};
+            }
+            else if (sum < target) {
+                i++;
+            }
+            else {
+                j--;
             }
         }
+
         return new int[]{};
     }
 }
